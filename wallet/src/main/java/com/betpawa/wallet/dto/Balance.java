49bd157818ed38a7ea14fc01259b4f2b6addeac3
JuliaKull <7102777@mail.ru>
2022-08-18 15:28:12 +0300

    created tables user and operation; added wallet-client;

--- wallet/src/main/java/com/betpawa/wallet/dto/Balance.java
+++ wallet/src/main/java/com/betpawa/wallet/dto/Balance.java
@@ -1,5 +1,7 @@
 package com.betpawa.wallet.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 public record Balance(Long accountId, BigDecimal amount) {
