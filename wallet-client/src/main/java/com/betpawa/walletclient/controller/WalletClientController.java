805432c1ca9e01ecb221fde4629e11ceafee981c
JuliaKull <7102777@mail.ru>
2022-08-19 08:28:06 +0300

    added methods addMoney and receiveMoney (service and controller layers) to wallet-client;

--- wallet-client/src/main/java/com/betpawa/walletclient/controller/WalletClientController.java
+++ wallet-client/src/main/java/com/betpawa/walletclient/controller/WalletClientController.java
@@ -5,7 +5,6 @@ import com.google.protobuf.Descriptors;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -16,21 +15,20 @@ public class WalletClientController {
 
     WalletClientService service;
 
-/*  @PostMapping("deposit/{account}")
-    public OperationResponse deposit(@PathVariable("account") Long account, DepositRequest request) {
-        final MoneyTransferResult moneyTransferResult = service.debitAccount(new MoneyTransferData(account, BigDecimal.valueOf(request.getAmount()), request.getReference()));
-        return new OperationResponse(moneyTransferResult.status().name(), moneyTransferResult.message());
+
+    @PostMapping("deposit/{account}")
+    public Map<Descriptors.FieldDescriptor, Object> reserveMoney(@PathVariable("account") Long account) {
+        return service.receiveMoney(account);
     }
 
     @PostMapping("withdraw/{account}")
-    public OperationResponse withdraw(@PathVariable("account") Long account, WithdrawRequest request) {
-        final MoneyTransferResult moneyTransferResult = service.creditAccount(new MoneyTransferData(account, BigDecimal.valueOf(request.getAmount()), request.getReference()));
-        return new OperationResponse(moneyTransferResult.status().name(), moneyTransferResult.message());
-    }*/
+    public Map<Descriptors.FieldDescriptor, Object> addMoney(@PathVariable("account") Long account) {
+        return service.addMoney(account);
+    }
 
     @GetMapping("balance/{account}")
-    public List<Map<Descriptors.FieldDescriptor,Object>> balance (@PathVariable("account") Long account) throws InterruptedException {
-       return service.balance(account);
-}
+    public List<Map<Descriptors.FieldDescriptor, Object>> balance(@PathVariable("account") Long account) throws InterruptedException {
+        return service.balance(account);
+    }
 
 }
--- wallet-client/src/main/java/com/betpawa/walletclient/service/WalletClientService.java
+++ wallet-client/src/main/java/com/betpawa/walletclient/service/WalletClientService.java
@@ -1,14 +1,11 @@
 package com.betpawa.walletclient.service;
 
-import com.betpawa.wallet.api.proto.AccountDetailsRequest;
-import com.betpawa.wallet.api.proto.BalanceResponse;
-import com.betpawa.wallet.api.proto.WalletGrpcServiceGrpc;
+import com.betpawa.wallet.api.proto.*;
 import com.google.protobuf.Descriptors;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -18,6 +15,10 @@ public class WalletClientService {
 
     @GrpcClient("grpc-wallet-service")
     WalletGrpcServiceGrpc.WalletGrpcServiceStub asynchronousClient;
+
+    @GrpcClient("grpc-wallet-service")
+    WalletGrpcServiceGrpc.WalletGrpcServiceBlockingStub synchronousClient;
+
 
     public List<Map<Descriptors.FieldDescriptor, Object>> balance(Long account) throws InterruptedException {
         final CountDownLatch countDownLatch = new CountDownLatch(1);
@@ -44,5 +45,15 @@ public class WalletClientService {
     }
 
 
+    public Map<Descriptors.FieldDescriptor, Object> receiveMoney(Long account) {
+        MoneyTransferRequest request = MoneyTransferRequest.newBuilder().setAccountId(account).build();
+        Iterator<MoneyTransferResponse> response = synchronousClient.reserveMoney(request);
+        return response.next().getAllFields();
+    }
 
+    public Map<Descriptors.FieldDescriptor, Object> addMoney(Long account) {
+        MoneyTransferRequest request = MoneyTransferRequest.newBuilder().setAccountId(account).build();
+        Iterator<MoneyTransferResponse> response = synchronousClient.addMoney(request);
+        return response.next().getAllFields();
+    }
 }
