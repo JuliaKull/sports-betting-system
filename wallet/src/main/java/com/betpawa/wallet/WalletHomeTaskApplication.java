cd2eb25322756411fd2ae9330f4c426cef1b43ec
JuliaKull <7102777@mail.ru>
2022-08-19 16:31:09 +0300

    added Swagger; localhost8083;

--- wallet/src/main/java/com/betpawa/wallet/WalletHomeTaskApplication.java
+++ wallet/src/main/java/com/betpawa/wallet/WalletHomeTaskApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EntityScan("com.betpawa.wallet.*")
-@EnableJpaRepositories("com.betpawa.wallet")
+@EnableSwagger2
 public class WalletHomeTaskApplication {
 
 	public static void main(String[] args) {
